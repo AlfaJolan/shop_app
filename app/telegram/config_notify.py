0b5304b18e5b52ca2b3d28f2bbec94c9cc33a0a4
Nurkhan <31109@iitu.edu.kz>
2025-09-19 18:31:27 +0500

    Очистка кэша и добавлен .gitignore

--- app/telegram/config_notify.py
+++ app/telegram/config_notify.py
@@ -1,7 +1,6 @@
 class NotifySettings:
     TELEGRAM_TOKEN = "8471566365:AAFqk7CHSdd8xXqW0aht-U3nwmSqwLjDR6M"
     TELEGRAM_ADMIN_PASSWORD = "testingApp"  # пароль для подписки
-    DB_PATH = "app.db"  # твоя основная база (или store_system.db)
 
 
 notify_settings = NotifySettings()
